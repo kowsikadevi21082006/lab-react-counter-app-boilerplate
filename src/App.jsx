5f254380c0a662d95f35dd920a740568c3016eda
kowsikadevi21082006 <deva36rp@gamil.com>
2023-12-28 14:59:30 +0530

    done

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+
+
+import React from "react";
+import './index.css'
+
+class Counter extends React.Component {
+
+  state={
+    count : 0,
+  }
+
+  Inc = () => {
+    this.setState({count: this.state.count + 1});
+  }
+
+  render() {
+
+    let Dec = () => {
+      if(this.state.count>0){
+      this.setState({count: this.state.count - 1});
+      }
+    }
+      let Reset = () =>{
+      this.setState( { count : this.state.count=0})
+     }
+
+    return (
+      <>
+        <h1>Counter App</h1>
+        <h1>{this.state.count}</h1>
+
+        <div>
+          <button onClick={this.Inc}>+</button>
+          <button onClick={Dec}>-</button>
+          <button onClick={Reset}>Reset</button>
+        </div>
+      </>
+    )
+  }
+}
+
+export default Counter;
